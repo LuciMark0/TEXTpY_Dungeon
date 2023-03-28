0ef2081f9f76f7d3838d993b50e19bddcfc6b75f
Göktuğ Kaan Koz <89668698+LuciMark0@users.noreply.github.com>
2023-03-28 17:11:36 +0300

    Update game_classes.py

--- game_classes.py
+++ game_classes.py
@@ -68,13 +68,13 @@ class Creature():
             return f"\n{self.name}'s info:\n\nReal stats: {self.real_stats}\nComplex stats: {self.complex_stats}\n\nPassive mysteries;\n{passive_mysts}\nActive mysteries;\n{active_mysts}"
 
     def set_conditions(self, mystery):
-        self.conditions.update({mystery.name: [mystery.target_stat_and_strength, mystery.turn_count]})
+        self.conditions.update({mystery.name: [mystery.target_stat_and_strength, mystery.turn_count, mystery.permanence, {"is_activated": False}]})
 
     def get_conditions(self):
         if not self.conditions:
             return "No conditions"
         else:
-            return "\n"+"\n".join([f"{key}: {value[0]} for remeaning {value[1]} turns" for key, value in self.conditions.items()])
+            return "\n"+"\n".join([f"{key}: {value[0]} for remeaning {value[1]} turns" + (" -temporarily-" if not value[2] else "") for key, value in self.conditions.items()])
 
 
     def change_weapon(self, taken_weapon):
@@ -128,37 +128,58 @@ class Creature():
         for mystery in self.passive_mysteries.values():
             if mystery.target == "self":
                 for target_stat, passive_strength in mystery.target_stat_and_strength.items():
-                    if target_stat in self.real_stats:
-                        target_stat_name_statement = "real_stats" if target_stat in self.real_stats else "complex_stats"
+                    
+                    target_stat_name_statement = "real_stats" if target_stat in self.real_stats else "complex_stats"
 
-                        if target_stat not in passive_stat_placeholders[target_stat_name_statement]:
-                            passive_stat_placeholders[target_stat_name_statement][target_stat] = passive_strength
-                        else:
-                            passive_stat_placeholders[target_stat_name_statement][target_stat] += passive_strength
+                    if target_stat not in passive_stat_placeholders[target_stat_name_statement]:
+                        passive_stat_placeholders[target_stat_name_statement][target_stat] = passive_strength
+                    else:
+                        passive_stat_placeholders[target_stat_name_statement][target_stat] += passive_strength
 
                 for stat_name, stat_value in passive_stat_placeholders.items():
                     for stat, value in stat_value.items():
                         self.__dict__[stat_name][stat] = value + self.base_stats[stat] if stat_name == "real_stats" else value + self.max_complex_stats[stat]
     
     def activate_conditions(self):
-        conditions_stat_placeholders = {"real_stats":{}, "complex_stats":{}} # to avoid changing stats while iterating
-        for mystery_name, condition in self.conditions.items():
-            for target_stat, strength in condition[0].items():
-                if target_stat in self.real_stats:
-                    self.real_stats[target_stat] = strength + self.base_stats[target_stat]
-                else:
-                    self.complex_stats[target_stat] = strength + self.max_complex_stats[target_stat]
-            else:
-                self.conditions.pop(mystery_name)
-        ...
+        if self.conditions:
+            conditions_stat_placeholders = {"real_stats":{}, "complex_stats":{}} # to avoid changing stats while iterating
+            for condition in self.conditions.values():
+                for target_stat, condition_strength in condition[0].items():
+                    if condition[3]["is_activated"] and not condition[2]:
+                        continue
+                    elif not condition[3]["is_activated"]:
+                        condition[3]["is_activated"] = True
+
+                    target_stat_name_statement = "real_stats" if target_stat in self.real_stats else "complex_stats"
+
+                    if target_stat not in conditions_stat_placeholders[target_stat_name_statement]:
+                            conditions_stat_placeholders[target_stat_name_statement][target_stat] = condition_strength
+                    else:
+                        conditions_stat_placeholders[target_stat_name_statement][target_stat] += condition_strength
+
+            for stat_name, stat_value in conditions_stat_placeholders.items():
+                for stat, value in stat_value.items():
+                    self.__dict__[stat_name][stat] = value + self.real_stats[stat] if stat_name == "real_stats" else value + self.complex_stats[stat]
+
+            finished_conditions = []
+            for mystery_name, condition in self.conditions.items():
+                condition[1] -= 1
+                if condition[1] == 0:
+                    finished_conditions.append({"mystery_name":mystery_name,"target_and_strength":condition[0],"permanency":condition[2]})
+
+            for condition in finished_conditions:
+                if not condition["permanency"]:
+                    print(condition["target_and_strength"])
+                    target_stat, condition_strength = next(iter(condition["target_and_strength"].items()))
+                    target_stat_name_statement = "real_stats" if target_stat in self.real_stats else "complex_stats"
+                    self.__dict__[target_stat_name_statement][target_stat] -= condition_strength
+
+                del self.conditions[condition["mystery_name"]]
+    ...
 
 
     
-    
-
-    
-                
-    
+        
 class Enemy(Creature):
     def __init__(self, name, vitality, aura_density, dexterity, constitution, prediction, weapon=None, mysteries=None) -> None:
         super().__init__(name, vitality, aura_density, dexterity, constitution, prediction, weapon, mysteries)
@@ -167,8 +188,6 @@ class Enemy(Creature):
     ...
     
     
-
-
 
 class Player(Creature):
     def __init__(self, name, vitality, aura_density, dexterity, constitution, prediction, weapon=None, mysteries=None) -> None:
@@ -214,7 +233,7 @@ pure_blood = Mystery("Pure Blood", "self", {"constitution":10}, 0, False)
 pure_soul = Mystery("Pure Soul", "self", {"aura_density":10}, 0, False)
 ticker_skin = Mystery("Ticker Skin","self", {"constitution":15}, 0, False)
 blackfire = Mystery("black fire", "enemy", {"health":-25}, 100, True, 3, True)
-fireball = Mystery("fire ball", "enemy", {"health":-10}, 50, True, 3, True)
+fireball = Mystery("fire ball", "enemy", {"health":-10}, 50, True, 3, False)
 quick_slice =  Mystery("quick slice", "enemy", {"health":-25}, 150, True)
 little_blessing = Mystery("little blessing", "self", {"health":50}, 50, True, 3, False)
 katana = Weapon("Katana", 5, [fireball])
@@ -233,6 +252,7 @@ def battle_system(player):
     turn = 0
     while True:
         turn += 1
+
         battle_queue = check_battle_queue(player, enemies)
 
         battle_ui(turn, player, battle_queue)
@@ -251,6 +271,7 @@ def check_battle_queue(player, enemies):
 
 
 def battle_ui(turn, player, battle_queue):
+    check_battlers_conditions(battle_queue)
     print(f"==========\n |turn {turn}|\n==========\n")
     print((f"{player.name} | Health: {player.complex_stats['health']} | Primordial Aura: {player.complex_stats['primordial_aura']} | Conditions: {player.get_conditions()}"))
     print("----------")
@@ -261,6 +282,11 @@ def battle_ui(turn, player, battle_queue):
     
     input("Press Enter to continue...")
 
+def check_battlers_conditions(battle_queue):
+    for battler in battle_queue:
+        if battler.conditions:
+            battler.activate_conditions()
+    ...
 
 def battle_action_system(turn, battle_queue):
 
@@ -326,7 +352,6 @@ def player_take_battle_action(player, enemy_queue):
 
 
 
-
 def check_is_int_and_len_longty(input, lenght = None):
     try:
         input = int(input)
@@ -337,6 +362,8 @@ def check_is_int_and_len_longty(input, lenght = None):
         print("\nInvalid input\n")
         return False
     return input
+
+
 
 player.forge_mystery_to_weapon([little_blessing, pure_blood])
 player.take_mystery([blackfire, pure_soul, quick_slice])
