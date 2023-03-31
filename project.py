3510e80e81cb3a610f96893618370eb644bfce02
Göktuğ Kaan Koz <89668698+LuciMark0@users.noreply.github.com>
2023-03-31 03:03:10 +0300

    Update project.py

--- project.py
+++ project.py
@@ -1,4 +1,5 @@
 from random import randint
+from game_classes import Mystery, Weapon, Player , Enemy
 # use tabulate to show stats
 
 def main():
@@ -10,17 +11,30 @@ def main():
     while True:
         event = movement_system()
         event_system(event)
+# Set mysteries
+pure_blood = Mystery("Pure Blood", "self", {"constitution":10}, 0, False)
+pure_soul = Mystery("Pure Soul", "self", {"aura_density":10}, 0, False)
+ticker_skin = Mystery("Ticker Skin","self", {"constitution":15}, 0, False)
+blackfire = Mystery("black fire", "enemy", {"health":-25}, 100, True, 3, True)
+fireball = Mystery("fire ball", "enemy", {"health":-10}, 50, True, 3, False)
+quick_slice =  Mystery("quick slice", "enemy", {"health":-85}, 150, True)
+little_blessing = Mystery("little blessing", "self", {"health":50}, 50, True, 3, False)
+# Set weapons
+katana = Weapon("Katana", 2, [fireball])
+great_katana = Weapon("Great Katana", 10, [blackfire])
+# Set player
+player = Player("player", 15, 10, 10, 50, 8, katana, [ticker_skin]) 
 
 def get_map(level):
     levels = ("""
       _____________________        
      │ !         !        !│_______
-┌────┘   ┌─────┐   ┌────┐ $   +  # 
+┌────┘   ┌─────┐   ┌────┐ *   +  # 
 │x     ? │     │   |____├──────────
 ├────┐   │_____│ ?             +  #
      │ !     ? ├───────────────────
      ├─────┐   │___________________
-           │ $                 +  #
+           │ *                 +  #
            ├───────────────────────
 """,
 """
@@ -59,7 +73,7 @@ def movement_system():
 
 def check_ways(mapd,player_location):
     walls = ("─","|","-","_","├","┘","┌","┐","┤")
-    events = ("?","!","$","#","+")
+    events = ("?","!","*","#","+")
 
     map_lines_len = len(mapd.split("\n")[0])+1 # +1 for splited "\n" character
 
@@ -116,11 +130,145 @@ def get_way_choice(available_ways):
 
     return choices[chosen_way]
         
-
+"""Events:
+    ? - Random event
+    + - Campfire event
+    ! - Normal battle
+    * - Elite battle
+    # - Boss battle
+"""
 def event_system(event):
+    if event == "!":
+        battle_system(player)
+
+def battle_system(player):
+        # create a random creature / Sparkle-Goat - stubbornness
+        enemies = []
+        for _ in range(2): #change that number for set difficulty
+            enemies.append(Enemy("Slime", 10, 20, 8, 50, 3, katana, [ticker_skin, quick_slice, blackfire]))
+
+        
+        turn = 0
+        while True:
+            turn += 1
+            enemies = check_healths(player, enemies)
+            if not enemies:
+                break
+            battle_queue = check_battle_queue(player, enemies)
+            battle_ui(turn, player, battle_queue)
+            battle_action_system(battle_queue, player)
+            
+def check_healths(player, enemies):
+    if player.complex_stats["health"] <= 0:
+        print("You died!")
+        exit()
+    else:
+        enemies = [enemy for enemy in enemies if enemy.complex_stats["health"] > 0]
+        if enemies == []:
+            print("You won!")
+            player.complex_stats["primordial_aura"] = player.max_complex_stats["primordial_aura"]
+            # add rewards etc.
+        return enemies
+
+def check_battle_queue(player, enemies):
+    battlers = [[player.complex_stats["initiative"], player]]
+
+    for enemy in enemies:
+        battlers.append([enemy.complex_stats["initiative"], enemy])
+
+    battle_queue = [battler for _,battler in sorted(battlers, key=lambda battler: battler[0], reverse=True)]
+    return battle_queue
+
+
+def battle_ui(turn, player, battle_queue):
+    check_battlers_conditions(battle_queue)
+    print(f"==========\n |turn {turn}|\n==========\n")
+    print((f"{player.name} | Health: {player.complex_stats['health']} | Primordial Aura: {player.complex_stats['primordial_aura']} | Conditions: {player.get_conditions()}"))
+    print("----------")
+    for enemy in battle_queue:
+        if enemy.__class__.__name__ == "Player":
+            continue
+        print(f"{enemy.name} | Health: {enemy.complex_stats['health']} | Primordial Aura: {enemy.complex_stats['primordial_aura']} | Conditions: {enemy.get_conditions()} \n")
+    
+    input("Press Enter to continue...")
+
+def check_battlers_conditions(battle_queue):
+    for battler in battle_queue:
+        if battler.conditions:
+            battler.activate_conditions()
     ...
 
+def battle_action_system(battle_queue, player):
 
+    #attack
+    for battler in battle_queue:
+        battler.aura_regen()
+        if battler.__class__.__name__ == "Player":
+            enemy_queue = [enemy for enemy in battle_queue if enemy.__class__.__name__ != "Player"]
+            battle_action_ui(battler, enemy_queue)
+
+        else:
+            selected_mystery = battler.select_mystery_for_battle(player)
+            print(f"{battler.name} used {selected_mystery.name}!")
+            battler.battle_action(player if selected_mystery.target != "self" else battler, selected_mystery)
+
+def battle_action_ui(player, enemy_queue):
+    while True:
+        choice = input("""
+        Take action:
+        1 => Show info
+        2 => Take Action
+
+        action: """)
+        print("----------")
+        if choice == "1":   
+            info_ui(player, enemy_queue)
+        elif choice == "2":
+            player_take_battle_action(player, enemy_queue)
+            break
+
+
+def info_ui(player, enemy_queue):
+    print(player.get_stats())
+    print("----------")
+    for enemy in enemy_queue:
+        print(enemy.get_stats(player.real_stats["prediction"]))
+        print("----------")
+    input("Press Enter to continue...")
+
+
+def player_take_battle_action(player, enemy_queue):
+    while True:
+        choice = check_is_int_and_len_longty(input("Choose an Action:\n"+player.get_stats(True)[0]+"\nAction: "), len(player.active_mysteries))
+        if choice:
+            chosen_mystery = player.get_stats(True)[1][choice-1]
+            if chosen_mystery.target == "enemy":
+                enemies = "".join([f"{count}==> {enemy.name}\n" for count, enemy in enumerate(enemy_queue, 1)])
+                while True:
+                    target = check_is_int_and_len_longty(input(f"Choose a target:\n{enemies}Target: "), len(enemy_queue))
+                    if target:
+                        successful_action = player.battle_action(enemy_queue[target-1], chosen_mystery)
+                        break
+            elif chosen_mystery.target == "self":
+                successful_action = player.battle_action(player, chosen_mystery)
+                
+            if successful_action == None:
+                break
+            else:
+                print("\nNot enough primordial aura!\n")
+
+
+
+def check_is_int_and_len_longty(input, lenght = None):
+    try:
+        input = int(input)
+        if lenght:
+            if not (0 < input <= lenght):
+                raise ValueError
+    except ValueError:
+        print("\nInvalid input\n")
+        return False
+    return input
 
 
 if __name__ == "__main__":
